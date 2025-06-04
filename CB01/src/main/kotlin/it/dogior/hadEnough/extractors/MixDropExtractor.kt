237acbac844aaeefee485b1f4fcea4df226447ac
federic0419 <135590399+federic0419@users.noreply.github.com>
2025-06-04 17:00:00 +0200

    Delete CB01 directory

--- CB01/src/main/kotlin/it/dogior/hadEnough/extractors/MixDropExtractor.kt
+++
@@ -1,7 +0,0 @@
-package it.dogior.hadEnough.extractors
-
-import com.lagradost.cloudstream3.extractors.MixDrop
-
-class MixDropExtractor : MixDrop(){
-    override var mainUrl = "https://mixdrop.ps"
-}
