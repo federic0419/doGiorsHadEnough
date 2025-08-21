ddc5f9eec3eeeeafedc0746526a08a1e453dc564
federic0419 <fede20022004@gmail.com>
2025-08-21 12:03:52 +0200

    aggiornato daddylive e calciostreaming

--- DaddyLive/src/main/kotlin/it/dogior/hadEnough/DaddyLiveExtractor.kt
+++ DaddyLive/src/main/kotlin/it/dogior/hadEnough/DaddyLiveExtractor.kt
@@ -3,15 +3,16 @@ package it.dogior.hadEnough
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.base64Decode
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.withContext
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.net.URL
 import java.net.URLEncoder
 
@@ -21,6 +22,10 @@ class DaddyLiveExtractor : ExtractorApi() {
     override val requiresReferer = false
     private val userAgent =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+    private val headers = mapOf(
+        "Referer" to mainUrl,
+        "user-agent" to userAgent
+    )
 
     override suspend fun getUrl(
         url: String,
@@ -41,24 +46,19 @@ class DaddyLiveExtractor : ExtractorApi() {
         }
     }
 
-    @Suppress("BlockingMethodInNonBlockingContext")
     private suspend fun extractVideo(url: String, sourceName: String = this.name): ExtractorLink? {
         if (!url.contains("daddylive")) return null
-        val headers = mapOf(
-            "Referer" to mainUrl,
-            "user-agent" to userAgent
-        )
-        val resp = app.post(url, headers = headers).body.string()
-        val url1 = Regex("iframe src=\"([^\"]*)").find(resp)?.groupValues?.get(1)
-            ?: return null
+
+        val resp = app.post(url, headers = headers).document
+        val iframes = resp.select("iframe")
+        val url1 = iframes.attr("src")
         val parsedUrl = URL(url1)
         val refererBase = "${parsedUrl.protocol}://${parsedUrl.host}"
-        val ref = URLEncoder.encode(refererBase, "UTF-8")
-        val userAgent = URLEncoder.encode(userAgent, "UTF-8")
 
-        val resp2 = app.post(url1, headers).body.string()
 
-        val finalUrl = extractFinalUrl(resp2, refererBase) ?: return null
+        val finalUrl = (if (url1.contains("vidembed")) extractFromVidembed(url1) else
+            extractFinalUrl(url1, refererBase)) ?: return null
+
 
         return newExtractorLink(
             sourceName,
@@ -66,58 +66,92 @@ class DaddyLiveExtractor : ExtractorApi() {
             finalUrl,
             type = ExtractorLinkType.M3U8,
         ) {
-            this.referer = ""
+            this.referer = "$refererBase/"
             this.quality = Qualities.Unknown.value
             this.headers = mapOf(
-                "Referer" to ref,
-                "Origin" to ref,
-                "Keep-Alive" to "true",
+                "Origin" to refererBase,
+                "Connection" to "Keep-Alive",
                 "User-Agent" to userAgent
             )
         }
 
     }
 
-    private suspend fun extractFinalUrl(page: String, serverUrl: String): String? {
+    private suspend fun extractFromVidembed(urlNextPage: String): String? {
+        val vidembedHost = urlNextPage.toHttpUrl().host
+        val liveId = urlNextPage.substringAfterLast("/").substringBefore("#")
+        val liveUrl = "https://www.$vidembedHost/api/source/$liveId?type=live"
+        val requestBody = "{\"r\":\"https://thedaddy.top/\",\"d\":\"www.$vidembedHost\"}"
+        val referer = if (urlNextPage.contains("//www.")) urlNextPage
+        else {
+            "https://www." + urlNextPage.substringAfter("https://").substringBefore("#")
+        }
+        val headers = mapOf(
+            "User-Agent" to userAgent,
+            "Referer" to referer,
+            "Origin" to referer.substringBefore("/stream"),
+            "X-Requested-With" to "XMLHttpRequest"
+        )
+        val resp = app.post(liveUrl, headers, referer = referer, json = requestBody).body.string()
+        val data = parseJson<VidembedResponse>(resp)
+        Log.d("DaddyLiveExtractor", data.toJson())
+        return null
+    }
+
+    private suspend fun extractFinalUrl(urlNextPage: String, serverUrl: String): String? {
+
+        val page = app.get(urlNextPage, headers).body.string()
         // Extracting security values
         val channelKeyRegex = "(?<=var channelKey = \").*(?=\")".toRegex()
-        val authTsRegex = "(?<=var authTs     = \").*(?=\")".toRegex()
-        val authRndRegex = "(?<=var authRnd    = \").*(?=\")".toRegex()
-        val authSigRegex = "(?<=var authSig    = \").*(?=\")".toRegex()
+        val authTsRegex = "(?<=var __c = atob.\").*(?=\")".toRegex()
+        val authRndRegex = "(?<=var __d = atob.\").*(?=\")".toRegex()
+        val authSigRegex = "(?<=var __e = atob.\").*(?=\")".toRegex()
 
 
         val channelKey = channelKeyRegex.find(page)?.value ?: return null
-        val authTs = authTsRegex.find(page)?.value ?: return null
-        val authRnd = authRndRegex.find(page)?.value ?: return null
-        val authSig = authSigRegex.find(page)?.value ?: return null
+//        Log.d("DDL", channelKey)
+        val authTs = base64Decode(authTsRegex.find(page)?.value ?: return null)
+//        Log.d("DDL", authTs)
+        val authRnd = base64Decode(authRndRegex.find(page)?.value ?: return null)
+//        Log.d("DDL", authRnd)
+        val authSig = base64Decode(authSigRegex.find(page)?.value ?: return null)
+//        Log.d("DDL", authSig)
 
         //Requests
+//        Log.d("DDL", "TRYING TO AUTH")
+        val h = mapOf(
+            "User-Agent" to userAgent,
+            "Referer" to "$serverUrl/",
+            "Origin" to serverUrl
+        )
         val authResponse = //withContext(Dispatchers.IO) {
             app.get(
                 "https://top2new.newkso.ru/auth.php?channel_id=" + channelKey +
                         "&ts=" + authTs +
                         "&rnd=" + authRnd +
-                        "&sig=" + URLEncoder.encode(authSig, "UTF-8")
-            ) //The response doesn't matter apparently
-        //}
-
-        val dataResponse = //withContext(Dispatchers.IO) {
-            app.get(
-                "$serverUrl/server_lookup.php?channel_id=${
-                    URLEncoder.encode(
-                        channelKey,
-                        "UTF-8"
-                    )
-                }"
+                        "&sig=" + URLEncoder.encode(authSig, "UTF-8"),
+                headers = h,
+//                interceptor = CloudflareKiller()
             )
         //}
-        val data = parseJson<DataResponse>(dataResponse.body.string())
-        val m3u8 = when (data.server_key) {
+//        Log.d("DDL", authResponse.code.toString())
+        Log.d("DDL", "Auth: " + authResponse.body.string())
+
+        val serverKey = app.get("$serverUrl/server_lookup.php?channel_id=$channelKey").body.string()
+        Log.d("DDL", "Server Key: $serverKey")
+        val data = parseJson<DataResponse>(serverKey)
+        //So far it works
+        val m3u8 = when (data.serverKey) {
             "top1/cdn" -> "https://top1.newkso.ru/top1/cdn/$channelKey/mono.m3u8"
-            else -> "https://${data.server_key}new.newkso.ru/${data.server_key}/$channelKey/mono.m3u8"
+            else -> "https://${data.serverKey}new.newkso.ru/${data.serverKey}/$channelKey/mono.m3u8"
         }
+        Log.d("DDL", "Final Url: $m3u8")
         return m3u8
     }
 
-    data class DataResponse(val server_key: String)
+    data class DataResponse(val serverKey: String)
+    data class VidembedResponse(
+        val success: Boolean,
+        val player: String
+    )
 }
